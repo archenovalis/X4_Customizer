89380e269c3b99bf892fa1862ec101e84cc500ed
archenovalis <archelaus.novalis@gmail.com>
2025-03-12 04:37:52 -0600

    bat file update

--- Cat_Unpack-modding.bat
+++ Cat_Unpack-modding.bat
@@ -1,3 +1,3 @@
-@echo of
-py "D:\Games\Steam\steamapps\common\X4 Foundations\X4_Customizer-1.24.12\Framework\Main.py" Cat_Unpack -argpass %* -include * -exclude *.dds *.xmf *.sig *.ani *.jcs *.ogg *.wav
+@echo off
+py "Framework\Main.py" Cat_Unpack -argpass %* -include * -exclude *.dds *.xmf *.sig *.ani *.jcs *.ogg *.wav *.gz *.xpm *.xsm *.xac *.f *.psb *.ogl *.h *.v *.abc *.jpg *.dtd *.pk *.comp *.vh *.fh *.tcs *.tes *.peb *.dae *.bgp *.bgf *.bsg *.amw
 pause
